dda0ce2a655d7c846d18dc35f0d6cc010512f3a2
Lauri Jalonen <ethereum108@gmail.com>
2020-10-29 08:24:13 +0800

    added basic store array and getter

---
+++ src/components/StudentsList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="students-list">
+      <h1>List of students</h1>
+      <div class="student" v-for="student in studentsArray" v-bind:key="student.id" >
+          Name: {{student.name}}, Age: {{student.age}}
+      </div>
+      <button>Add Student</button>
+  </div>
+</template>
+
+<script>
+
+import { mapGetters } from 'vuex'
+
+export default {
+
+  computed: {
+    ...mapGetters(['studentsArray'])
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.student {
+    background-color: aquamarine
+}
+
+</style>>
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    centralCounter: 1,
+    centralMessage: 'Vuex string',
+    studentsArray: [
+      { id: 'zxc1', name: 'Jack', age: 1 },
+      { id: 'zxc2', name: 'Jill', age: 2 },
+      { id: 'zxc3', name: 'Rick', age: 3 },
+      { id: 'zxc4', name: 'Tracy', age: 5 }
+    ]
   },
   mutations: {
+    addStudent (state, student) {
+      state.studentsArray.push(student)
+    }
   },
   actions: {
   },
   modules: {
+  },
+  getters: {
+    studentsArray: state => { return state.studentsArray }
   }
 })
